caf0263ae729ad73207ad028c26160a796df0375
Insu Byeon <yuki@yuki.kr>
2023-01-30 20:27:02 +0900

    🩸 First blood

---
+++ InjectHotReloading/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InjectHotReloading
+//
+//  Created by Insu Byeon on 2023/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "InjectHotReloading"
+        view.backgroundColor = .white
+    }
+
+}
+
